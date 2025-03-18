4376e0a42461a7be2a94d6d5773eea675e233827
dan <dantselish0@gmail.com>
2025-03-18 23:06:14 +0200

    3rd-person camera and input adjusted

--- Assets/ProjectFiles/Scripts/PlayerMovement/PlayerBasicController.cs
+++ Assets/ProjectFiles/Scripts/PlayerMovement/PlayerBasicController.cs
@@ -1,18 +1,28 @@
-﻿using UnityEngine;
+﻿using Unity.Cinemachine;
+using UnityEngine;
 
 
 public class PlayerBasicController : MonoBehaviour, IInputObserver
 {
     [Header("References")]
     [SerializeField] private Transform movementTransform;
+    [SerializeField] private Transform cameraTarget;
     [SerializeField] private PlayerAnimator playerAnimator;
+    [SerializeField] private GameObject playerCameraPrefab;
 
     [Header("Settings")]
     [SerializeField] private float walkingSpeed;
     [SerializeField] private float runningSpeed;
+    [SerializeField] private float rotationSpeed;
 
     private InputValues _inputValues;
+    private Camera _playerCamera;
 
+
+    private void Awake()
+    {
+        SpawnCamera();
+    }
 
     private void Start()
     {
@@ -53,9 +63,17 @@ public class PlayerBasicController : MonoBehaviour, IInputObserver
             return;
         }
 
+        Vector3 cameraForward = _playerCamera.transform.forward;
+        cameraForward.y = 0;
+        cameraForward.Normalize();
+
+        Vector3 cameraRight = _playerCamera.transform.right;
+        cameraRight.y = 0;
+        cameraRight.Normalize();
+
         Vector3 translationVector = Vector3.zero;
-        translationVector += Vector3.forward * inputVector.y;
-        translationVector += Vector3.right * inputVector.x;
+        translationVector += cameraForward * inputVector.y;
+        translationVector += cameraRight * inputVector.x;
 
         if (_inputValues.sprint)
         {
@@ -70,6 +88,30 @@ public class PlayerBasicController : MonoBehaviour, IInputObserver
             translationVector *= walkingSpeed * Time.deltaTime;
         }
 
-        movementTransform.Translate(translationVector);
+        movementTransform.Translate(translationVector, Space.World);
+        Quaternion lookRotation = Quaternion.LookRotation(translationVector);
+        movementTransform.rotation = Quaternion.Slerp(movementTransform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private void SpawnCamera()
+    {
+        GameObject cameraGO = Instantiate(playerCameraPrefab);
+        _playerCamera = cameraGO.GetComponentInChildren<Camera>();
+
+        if (!_playerCamera)
+        {
+            Debug.LogError($"No {nameof(Camera)} found!");
+            return;
+        }
+
+        CinemachineCamera cinemachineCamera = cameraGO.GetComponentInChildren<CinemachineCamera>();
+
+        if (!cinemachineCamera)
+        {
+            Debug.LogError($"No {nameof(CinemachineCamera)} found!");
+            return;
+        }
+
+        cinemachineCamera.Target.TrackingTarget = cameraTarget;
     }
 }
